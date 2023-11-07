2f80713d755930a2a7874ff2b4db474f6b9768a7
Diuri <diuri.student@gmail.com>
2023-11-07 00:00:10 -0300

    add image acording to weather condition // add climate images export file

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,11 +1,18 @@
 import { useContext } from "react";
 import { CurrentCityContext } from '../../contexts/CurrentCityContext.js';
+import climateImages from "../../images/climates/climatesImageExport.js";
 
 export default function currentWeather() {
   const currentCity = useContext(CurrentCityContext);
 
-  console.log(currentCity)
   return (
-    <div>CurrentWeather</div>
+    <>
+      <div>
+        <h1 className="currentWeather__city_name"></h1>
+        <h2 className="currentWeather__rain_chance"></h2>
+        <p className="currentWeather__degree"></p>
+      </div>
+      <img className="currentWeather__weather_condition" src={climateImages[currentCity.condition]} />
+    </>
   )
 }
