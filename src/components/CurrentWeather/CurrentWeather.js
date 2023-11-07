eecc6e889cea4ffe19d91dca9144e8ede8aa689e
Diuri <diuri.student@gmail.com>
2023-11-07 15:33:45 -0300

    add image according to moon phase // add moon phase images export file

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -1,18 +1,19 @@
 import { useContext } from "react";
 import { CurrentCityContext } from '../../contexts/CurrentCityContext.js';
 import climateImages from "../../images/climates/climatesImageExport.js";
+import moonPhasesImages from "../../images/moon_phases/moonPhaseImagesExport.js";
 
 export default function currentWeather() {
   const currentCity = useContext(CurrentCityContext);
 
   return (
-    <>
-      <div>
-        <h1 className="currentWeather__city_name"></h1>
-        <h2 className="currentWeather__rain_chance"></h2>
-        <p className="currentWeather__degree"></p>
+    <div className="current-weather">
+      <img className="current-weather__weather-condition" src={climateImages[currentCity.condition]} />
+      <div className="current-weather__container">
+        <h1 className="current-weather__city-name">{currentCity.city}</h1>
+        <p className="current-weather__temperature">{currentCity.temp}</p>
       </div>
-      <img className="currentWeather__weather_condition" src={climateImages[currentCity.condition]} />
-    </>
+      <img className="current-weather__moon-phase" src={moonPhasesImages[currentCity.moon_phase]} />
+    </div>
   )
 }
---
+++ src/images/moon_phases/moonPhaseImagesExport.js
@@ -0,0 +1,21 @@
+import firstQuarter from './first_quarter.png';
+import full from './full.png';
+import lastQuarter from './last_quarter.png';
+import newMoon from './new.png';
+import waningCrescent from './waning_crescent.png';
+import waningGibbous from './waning_gibbous.png';
+import waxingCrescent from './waxing_crescent.png';
+import waxingGibbous from './waxing_gibbous.png';
+
+const moonPhasesImages = {
+  'first_quarter': firstQuarter,
+  'full': full,
+  'last_quarter': lastQuarter,
+  'new': newMoon,
+  'waning_crescent': waningCrescent,
+  'waning_gibbous': waningGibbous,
+  'waxing_crescent': waxingCrescent,
+  'waxing_gibbous': waxingGibbous
+}
+
+export default moonPhasesImages;
