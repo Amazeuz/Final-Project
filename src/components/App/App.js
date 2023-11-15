c50d13bf679372fab6c00d9b4b5dabbf24aa3845
Diuri <diuri.student@gmail.com>
2023-11-15 14:57:47 -0300

    removed defaultCity file, set São Paulo as default city with useEffect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,37 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Route, HashRouter, Switch } from 'react-router-dom';
 import Header from '../Header/Header.js';
 import ApiData from '../ApiData/ApiData.js';
 import Main from '../Main/Main.js';
 import Footer from '../Footer/Footer.js';
+import PreLoader from '../PreLoader/PreLoader.js';
 import AboutPopup from '../AboutPopup/AboutPopup.js';
-import defaultCity from '../../utils/defaultCity.js';
 import api from '../../utils/api.js';
 import { CurrentCityContext } from '../../contexts/CurrentCityContext.js';
 
 export default function App() {
-  const [currentCity, setCurrentCity] = useState(defaultCity);
-  const [isAboutPopupOpen, setAboutPopupState] = useState(false)
+  const [currentCity, setCurrentCity] = useState('');
+  const [isAboutPopupOpen, setAboutPopupState] = useState(false);
+  const [isPageLoaded, setPageLoaded] = useState(true);
 
-  function searchCity(city) {
-    api.getCityInfo(city)
+  useEffect(() => {
+    searchCity('São Paulo')
+  }, [])
+
+  async function searchCity(city) {
+    setPageLoaded(false)
+    await api.getCityInfo(city)
       .then((data) => {
         setCurrentCity(data)
       })
+    setPageLoaded(true)
   }
 
   return (
     <CurrentCityContext.Provider value={currentCity}>
       {isAboutPopupOpen && <AboutPopup setAboutPopupState={setAboutPopupState} />}
-      <div className={`page ${isAboutPopupOpen && 'page_overlay'}`}>
+      {!isPageLoaded && <PreLoader />}
+      <div className={`page ${isAboutPopupOpen | !isPageLoaded && 'page_overlay'}`}>
         <HashRouter>
           <Header setAboutPopupState={setAboutPopupState} />
           <Switch>
---
+++ src/components/PreLoader/PreLoader.js
@@ -0,0 +1,5 @@
+export default function PreLoader() {
+  return (
+    <i class="circle-preloader"></i>
+  )
+}
